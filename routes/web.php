f7c1b13d7f7315283918daef315091558659a6ba
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-09-03 11:32:01 -0300

    fix: updated application routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LanguageController;
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout.base', [
+        'page' => 'home',
+    ]);
 });
