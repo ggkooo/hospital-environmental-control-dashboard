482a7c2c59c03b9a9cd94ef8dd362037fa808f7b
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-09-03 11:32:27 -0300

    feat: created all idioms sidebar variables

---
+++ resources/lang/hi/sidebar.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'home' => 'Home',
+    'temperature' => 'Temperature',
+    'humidity' => 'Humidity',
+    'noise' => 'Noise',
+    'report' => 'Report',
+    'administration' => 'Administration',
+    'user_management' => 'User Management',
+    'report_generation' => 'Report Generation',
+    'access_control' => 'Access Control',
+    'user_name' => 'User Name',
+    'leave' => 'Leave',
+];
---
+++ resources/lang/pt-br/sidebar.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'home' => 'Home',
+    'temperature' => 'Temperature',
+    'humidity' => 'Humidity',
+    'noise' => 'Noise',
+    'report' => 'Report',
+    'administration' => 'Administration',
+    'user_management' => 'User Management',
+    'report_generation' => 'Report Generation',
+    'access_control' => 'Access Control',
+    'user_name' => 'User Name',
+    'leave' => 'Leave',
+    'dashboard' => 'Painel',
+    'patients' => 'Pacientes',
+    'rooms' => 'Salas',
+    'environment' => 'Ambiente',
+    'reports' => 'Relatórios',
+    'settings' => 'Configurações',
+    'logout' => 'Sair',
+];
