6b3ca075a610352fd49d9c561b8ddb52cbd8cac3
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-09-03 11:31:46 -0300

    feat: created the home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+<div class="container mt-5">
+    <h1 class="text-center mb-4">Dashboard de Controle Ambiental Hospitalar</h1>
+    <p class="lead text-center text-dark">Bem-vindo ao sistema de monitoramento ambiental do hospital. Aqui você pode acompanhar indicadores de qualidade do ar, temperatura, umidade e outros parâmetros essenciais para a segurança e bem-estar dos pacientes e colaboradores.</p>
+    <div class="text-center mt-4">
+        <a href="#" class="btn btn-primary btn-lg">Ver Indicadores</a>
+    </div>
+</div>
