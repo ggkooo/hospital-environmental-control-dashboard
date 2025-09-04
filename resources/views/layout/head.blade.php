c98703f199b1f130d6d100d19139644c19ac47a0
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-09-04 08:48:48 -0300

    fix: updated application head, including bootstrap icons

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -9,6 +9,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/css/bootstrap-select.min.css">
     <!-- Bootstrap CSS (se ainda não estiver incluído) -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <!-- Bootstrap Icons CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 
     <!-- jQuery -->
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
