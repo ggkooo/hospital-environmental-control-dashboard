ffe2f0b6e21d23938e847adf786944478a6d59fa
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-09-04 08:50:54 -0300

    fix: updated webpack.mix.cjs and mix-manifest.json

--- webpack.mix.cjs
+++ webpack.mix.cjs
@@ -2,4 +2,8 @@
 const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+
+    // SIDEBAR
+    .js('resources/js/layout/sidebar.js', 'public/js/layout')
+    .sass('resources/sass/layout/sidebar.scss', 'public/css/layout');
