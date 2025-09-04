d06b363d81c51688e4ab47ed07ba7e3b8a0e953a
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-09-04 09:04:28 -0300

    fix: updated webpack.mix.cjs and mix-manifest.json

--- webpack.mix.cjs
+++ webpack.mix.cjs
@@ -6,4 +6,8 @@ mix.js('resources/js/app.js', 'public/js')
 
     // SIDEBAR
     .js('resources/js/layout/sidebar.js', 'public/js/layout')
-    .sass('resources/sass/layout/sidebar.scss', 'public/css/layout');
+    .sass('resources/sass/layout/sidebar.scss', 'public/css/layout')
+    
+    // HEADER
+    .js('resources/js/layout/header.js', 'public/js/layout')
+    .sass('resources/sass/layout/header.scss', 'public/css/layout');
