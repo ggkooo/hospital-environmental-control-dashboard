abc344071ed7a1ff166d34fa9105290086769a4f
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-09-05 09:51:32 -0300

    feat: created all supported temperature page translate

---
+++ resources/lang/tr/sidebar.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'home' => 'Ana Sayfa',
+    'temperature' => 'Sıcaklık',
+    'humidity' => 'Nem',
+    'noise' => 'Gürültü',
+    'report' => 'Rapor',
+    'administration' => 'Yönetim',
+    'user_management' => 'Kullanıcı Yönetimi',
+    'report_generation' => 'Rapor Oluşturma',
+    'access_control' => 'Erişim Kontrolü',
+    'user_name' => 'Kullanıcı Adı',
+    'leave' => 'Çıkış Yap',
+];
