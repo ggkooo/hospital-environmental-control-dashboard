fc14f0479ec355a649f167ff8f1e92e208dd47ea
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-09-22 14:35:31 -0300

    feat: created the login controller

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\auth\LoginRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+
+class LoginController extends Controller
+{
+    public function showLoginForm(): View
+    {
+        return view('auth', [
+            'action' => 'login',
+        ]);
+    }
+
+    public function store(LoginRequest $request): RedirectResponse
+    {
+        $validated = $request->validated();
+
+        if (auth()->attempt($validated)) {
+            $request->session()->regenerate();
+            return redirect()->intended(route('home'));
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
+}
