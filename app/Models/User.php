ba1166e3c4abbf9c53f2f2bf2ddf8760e5388505
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-09-22 15:39:31 -0300

    fix: updated user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_admin',
+        'is_validated',
     ];
 
     /**
@@ -43,6 +45,8 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_admin' => 'boolean',
+            'is_validated' => 'boolean',
         ];
     }
 }
