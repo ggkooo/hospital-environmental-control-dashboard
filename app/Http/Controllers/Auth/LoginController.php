8c9465c0f39396119b78e140f12cb09db2fd12d9
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-09-22 15:11:52 -0300

    feat: created the logout method

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -29,4 +29,12 @@ class LoginController extends Controller
             'email' => 'The provided credentials do not match our records.',
         ])->onlyInput('email');
     }
+
+    public function logout(): RedirectResponse
+    {
+        auth()->logout();
+        request()->session()->invalidate();
+        request()->session()->regenerateToken();
+        return redirect()->route('login.form');
+    }
 }
