e102657b50cfcaabf698757cb59cd963042faaeb
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-09-22 21:30:53 -0300

    feat: created api endpoints to get temperature data

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\ApiController;
+use App\Http\Controllers\TemperatureController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/', [ApiController::class, 'handle']);
+
+Route::get('temperature', [TemperatureController::class, 'index']);
+Route::get('temperature/available-dates', [TemperatureController::class, 'availableDates']);
